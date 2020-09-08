b58223a604aae26985c3d2826203713dac02f680
Keshav <keshurathore99@gmail.com>
2020-09-08 14:40:38 +0530

    fashion page updated

--- lib/widgets/second_product_item_for_fashion_screen.dart
+++ lib/widgets/second_product_item_for_fashion_screen.dart
@@ -1,4 +1,5 @@
 import 'package:delilo/models/product.dart';
+import 'package:delilo/screens/home/productdetailspage.dart';
 import 'package:flutter/material.dart';
 
 class SecondProductItemForFashionScreen extends StatelessWidget {
@@ -7,41 +8,47 @@ class SecondProductItemForFashionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Card(
-          elevation: 5,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: FadeInImage(
-              placeholder: AssetImage('assets/fashion/fashion7.jpg'),
-              image: NetworkImage(product.imageUrl),
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ProductDetailPage(product: product)));
+      },
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Card(
+            elevation: 5,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/fashion/fashion7.jpg'),
+                image: NetworkImage(product.imageUrl),
+              ),
             ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          decoration: BoxDecoration(
-              color: Colors.green, borderRadius: BorderRadius.circular(15)),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                product.productName,
-                style: TextStyle(color: Colors.white),
-              ),
-              Text(product.shopName,
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold)),
-              Text('₹ ' + product.price.toString(),
-                  style: TextStyle(color: Colors.white)),
-            ],
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+                color: Colors.green, borderRadius: BorderRadius.circular(15)),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  product.productName,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(product.shopName,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                Text('₹ ' + product.price.toString(),
+                    style: TextStyle(color: Colors.white)),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
