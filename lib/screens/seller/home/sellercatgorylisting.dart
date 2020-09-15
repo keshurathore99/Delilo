bd3c10ba8b74c1b1b2261ec9a4f808e20cba4a2c
Keshav <keshurathore99@gmail.com>
2020-09-15 20:14:42 +0530

    UI Changes in Seller Category Listing Screen

--- lib/screens/seller/home/sellercatgorylisting.dart
+++ lib/screens/seller/home/sellercatgorylisting.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:delilo/screens/seller/home/newproductpage.dart';
 import 'package:delilo/screens/seller/home/sellerdrawer.dart';
+import 'package:delilo/screens/seller/home/sellerhomesceen.dart';
 import 'package:delilo/seller%20widgets/category_listing_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:delilo/screens/auxillary/customclasses.dart';
@@ -47,22 +48,62 @@ class _SellerCategoryListingState extends State<SellerCategoryListing> {
           );
         }),
       ),
-      body: ListView.builder(
-          itemCount: widget.uploadedProducts.length,
-          itemBuilder: (context, index) {
-            return FutureBuilder<DocumentSnapshot>(
-                future: Firestore.instance
-                    .document(widget.uploadedProducts[index])
-                    .get(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Container();
-                  }
-                  final snap = snapshot.data.data;
-                  return CategoryListingTile(
-                      snap, widget.uploadedProducts[index], _scaffoldKey);
-                });
-          }),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                FlatButton(
+                  child: Text('All Items'),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  textColor: Colors.green,
+                  color: Colors.green.withOpacity(0.2),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => SellerCategoryListing(
+                            uploadedProducts: widget.uploadedProducts)));
+                  },
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                FlatButton(
+                  child: Text('Out of Stock'),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  textColor: Colors.black87,
+                  color: Colors.black12,
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => OutOfStock()));
+                  },
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: widget.uploadedProducts.length,
+                itemBuilder: (context, index) {
+                  return FutureBuilder<DocumentSnapshot>(
+                      future: Firestore.instance
+                          .document(widget.uploadedProducts[index])
+                          .get(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Container();
+                        }
+                        final snap = snapshot.data.data;
+                        return CategoryListingTile(
+                            snap, widget.uploadedProducts[index], _scaffoldKey);
+                      });
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
